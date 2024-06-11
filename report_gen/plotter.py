1569105386f4d37fe9417d0b58ddedb3a8e65d8c
thilinawee <thilinaholmes@gmail.com>
2024-06-11 14:53:58 +1000

    Plot data by averaging values across seeds

--- report_gen/plotter.py
+++ report_gen/plotter.py
@@ -1,27 +1,83 @@
 import json
+import os
+
 import matplotlib.pyplot as plt
 
 class JSONDataPlotter:
-    def __init__(self, file1, file2):
+    FILE_1 = "sota"
+    FILE_2 = "novel"
+    ORIGINAL_ACC = "original_acc"
+    ADAPTED_ACC = "adapted_acc"
+    PARTIAL_ACC = "partial_acc"
+
+    def __init__(self, reports_dir, dataset, batch_size, n_drop_classes, severity):
+        self.reports_dir = reports_dir
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.n_drop_classes = n_drop_classes
+        self.severity = severity
+
+        self._data1 = None
+        self._data2 = None
+
+        self._get_avg_data()
+
+    def _get_data_dir(self):
+        data_dir = f"{self.reports_dir}/{self.dataset}/bs{self.batch_size}_dc{self.n_drop_classes}_s{self.severity}"
+
+        if not os.path.isdir(data_dir):
+            raise Exception(f"Directory {data_dir} does not exist")
+        
+        return data_dir
+    
+    def _get_avg_data(self):
+        # average all the data accross seeds
+        seed_dirs = os.listdir(self._get_data_dir())
+        if len(seed_dirs) == 0:
+            raise Exception(f"No seed directories found in {self._get_data_dir()}")
+        
+        data_list1 = []
+        data_list2 = []
+
+        for seed_dir in seed_dirs:
+            file_1 = f"{self._get_data_dir()}/{seed_dir}/{self.FILE_1}.json"
+            file_2 = f"{self._get_data_dir()}/{seed_dir}/{self.FILE_2}.json"
+
+            with open(file_1) as f1:
+                data1 = json.load(f1)
+                data_list1.append(data1)
+            with open(file_2) as f2:
+                data2 = json.load(f2)
+                data_list2.append(data2)
+        
+        self._data1 = self._avg_values(data_list1)
+        self._data2 = self._avg_values(data_list2)
+
+    def _avg_values(self, data_list):
         """
-        Parameters
-        ----------
-        file1 : str : Path to outputs of SOTA environment
-        file2 : str : Path to outputs of our environment
+        Get the avg value of all the data fields across multiple files
         """
-        self.file1 = file1
-        self.file2 = file2
+        avg_data = {}
 
-        self._data1, self._data2 = self._load_data()
+        for data in data_list:
+            for key, value in data.items():
+                if key not in avg_data:
+                    avg_data[key] = {}
 
-    def _load_data(self):
-        # Load data from JSON files
-        with open(self.file1) as f1:
-            data1 = json.load(f1)
-        with open(self.file2) as f2:
-            data2 = json.load(f2)
-        return data1, data2
+                for sub_key, sub_value in value.items():
+                    if sub_key not in avg_data[key]:
+                        avg_data[key][sub_key] = 0
 
+                    avg_data[key][sub_key] += sub_value
+
+        n_files = len(data_list)
+        
+        for key, value in avg_data.items():
+            for sub_key, sub_value in value.items():
+                avg_data[key][sub_key] = sub_value / n_files
+
+        return avg_data
+                    
     def plot_data_for_all_labels(self):
 
         # Extract x and y values from data
